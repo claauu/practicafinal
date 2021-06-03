20ab3d17b0643ea602da70108f85ff4e0340855f
claauu <claudiaragones9@gmail.com>
2021-06-03 02:23:50 +0200

    pprueb

--- script_manager.py
+++ script_manager.py
@@ -55,34 +55,34 @@ def script_response(script):
 
 def pichichi():
 
-    url = 'https://www.marca.com/futbol/primera-division/pichichi.html'
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    # url = 'https://www.marca.com/futbol/primera-division/pichichi.html'
+    # page = requests.get(url)
+    # soup = BeautifulSoup(page.content, 'html.parser')
 
-    #Jugadores - Equipo
-    jug = soup.find_all('th', class_='ue-table-trophies__th is-main')
+    # #Jugadores - Equipo
+    # jug = soup.find_all('th', class_='ue-table-trophies__th is-main')
 
-    jugadores = list()
+    # jugadores = list()
 
-    for i in jug:
-        jugadores.append(i.text)
+    # for i in jug:
+    #     jugadores.append(i.text)
 
-    #print(jugadores)
+    # #print(jugadores)
 
-    #Goles totales
-    gol = soup.find_all('td', class_='ue-table-trophies__td is-marked')
+    # #Goles totales
+    # gol = soup.find_all('td', class_='ue-table-trophies__td is-marked')
 
-    goles = list()
+    # goles = list()
 
-    for i in gol:
-        goles.append(i.text)
+    # for i in gol:
+    #     goles.append(i.text)
 
-    #print(goles)
+    # #print(goles)
 
 
-    #print(goles)
-    df = pd.DataFrame({'Jugador (equipo)': jugadores, 'Goles': goles}, index=list(range(1,51)))
-    print(df)
-    return "pichihi"
-    #return df
+    # #print(goles)
+    # df = pd.DataFrame({'Jugador (equipo)': jugadores, 'Goles': goles}, index=list(range(1,51)))
+    # print(df)
+   
+    return "hola"
     #df.to_csv('Pichichi30.csv')
