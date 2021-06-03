b1fea0d17fed6351f3e64c64da04f8fc0168b5c1
claauu <claudiaragones9@gmail.com>
2021-06-03 02:35:48 +0200

    p5

--- script_manager.py
+++ script_manager.py
@@ -5,41 +5,41 @@ import TopInstagram
 import TopYouTube
 import Pichichi
 import Zamora
-from bs4 import  BeautifulSoup
+from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 from datetime import datetime
 
 def pichichi():
 
-    url = 'https://www.marca.com/futbol/primera-division/pichichi.html'
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    # url = 'https://www.marca.com/futbol/primera-division/pichichi.html'
+    # page = requests.get(url)
+    # soup = BeautifulSoup(page.content, 'html.parser')
 
-    #Jugadores - Equipo
-    jug = soup.find_all('th', class_='ue-table-trophies__th is-main')
+    # #Jugadores - Equipo
+    # jug = soup.find_all('th', class_='ue-table-trophies__th is-main')
 
-    jugadores = list()
+    # jugadores = list()
 
-    for i in jug:
-        jugadores.append(i.text)
+    # for i in jug:
+    #     jugadores.append(i.text)
 
-    #print(jugadores)
+    # #print(jugadores)
 
-    #Goles totales
-    gol = soup.find_all('td', class_='ue-table-trophies__td is-marked')
+    # #Goles totales
+    # gol = soup.find_all('td', class_='ue-table-trophies__td is-marked')
 
-    goles = list()
+    # goles = list()
 
-    for i in gol:
-        goles.append(i.text)
+    # for i in gol:
+    #     goles.append(i.text)
 
-    #print(goles)
+    # #print(goles)
 
 
-    #print(goles)
-    df = pd.DataFrame({'Jugador (equipo)': jugadores, 'Goles': goles}, index=list(range(1,51)))
-    print(df)
+    # #print(goles)
+    # df = pd.DataFrame({'Jugador (equipo)': jugadores, 'Goles': goles}, index=list(range(1,51)))
+    # print(df)
    
     return 1
     #df.to_csv('Pichichi30.csv')
@@ -69,7 +69,6 @@ def script_response(script):
 
     if script == "script_pichichi" or script=="quien es el pichichi":  
         df=pichichi()
-        print(df)
         return f"Estos son los maximos goleadores: <br> {df}"  
 
     if script == "script_youtube" or script=="quienes son los mas seguidos en youtube": 
